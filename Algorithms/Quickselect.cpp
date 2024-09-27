07c7ce9a319448391bb99e26f438bbddb607fe2a
Kaunik Kamila <kaunikkamila01@gmail.com>
2024-09-27 06:31:32 -0700

    modified QuickSelect algorithm

--- Algorithms/Quickselect.cpp
+++ Algorithms/Quickselect.cpp
@@ -1,3 +1,26 @@
+/* 
+    Time Complexity
+    - Average Case: O(n), where n is the number of elements in the array. This is because, on average, each partition reduces the size of the problem significantly (about half).
+    - Worst Case: O(n²), which occurs when the smallest or largest element is consistently chosen as the pivot (like in sorted or reverse-sorted arrays). This can be mitigated by using a random pivot or other strategies.
+
+    How Quickselect Works
+        Selection of Pivot:
+            - Just like Quicksort, Quickselect begins by selecting a pivot element from the array. This pivot can be chosen randomly or using some heuristic (like the median of medians). The choice of pivot affects performance, but Quickselect is designed to work efficiently on average.
+        
+        Partitioning:
+            The array is then partitioned into three segments relative to the pivot:
+            -   Left: Elements less than the pivot.
+            -   Right: Elements greater than the pivot.
+            -   Middle: Elements equal to the pivot.
+            After partitioning, the pivot element will be in its final sorted position.
+        
+        Recursive Search:
+            Depending on the position of the pivot after partitioning:
+            -   If the pivot’s position matches the desired k-th index, return the pivot.
+            -   If the k-th index is less than the pivot's position, recursively call Quickselect on the left partition.
+            -   If the k-th index is greater than the pivot's position, recursively call Quickselect on the right partition, adjusting k accordingly.
+*/
+
 #include <iostream>
 #include <vector>
 #include <cstdlib>  
@@ -43,41 +66,37 @@ public:
         
     }
 
-
-
     // This function is space optimized, no need to create 3 vectors
     int quickSelectWithSpaceOptimization(std::vector<int>& nums, int left, int right, int k) {
-        if (left == right) {
-            return nums[left];  // If the list contains only one element
+    // Choose the last element as the pivot
+        int pivot = nums[right];
+        int partitionIndex = partition(nums, left, right, pivot);
+
+        // If the partition index matches k, we found the kth element
+        if (partitionIndex == k) {
+            return nums[partitionIndex];
         }
+        // If k is less than the partition index, search left side
+        else if (k < partitionIndex) {
+            return quickSelect(nums, left, partitionIndex - 1, k);
+        }
+        // If k is greater than the partition index, search right side
+        else {
+            return quickSelect(nums, partitionIndex + 1, right, k);
+        }
+    }
 
-        // Choose a random pivot index between left and right
-        int pivotIndex = left + rand() % (right - left + 1); // or can simply take last value as pivot
-        int pivotValue = nums[pivotIndex];
-
-        // Move pivot to the end
-        std::swap(nums[pivotIndex], nums[right]);
-
-        // Partitioning
-        int storeIndex = left;
-        for (int i = left; i < right; ++i) {
-            if (nums[i] < pivotValue) {
-                std::swap(nums[storeIndex], nums[i]);
-                storeIndex++;
+    int partition(std::vector<int>& nums, int left, int right, int pivot) {
+        int i = left;
+        for (int j = left; j < right; j++) {
+            if (nums[j] < pivot) {
+                std::swap(nums[i], nums[j]);
+                i++;
             }
         }
-
-        // Move pivot to its final place
-        std::swap(nums[storeIndex], nums[right]);
-
-        // Recursion: we can now determine the position of the pivot
-        if (k == storeIndex) {
-            return nums[storeIndex];
-        } else if (k < storeIndex) {
-            return quickSelect(nums, left, storeIndex - 1, k);
-        } else {
-            return quickSelect(nums, storeIndex + 1, right, k);
-        }
+        // Place the pivot in its correct position
+        std::swap(nums[i], nums[right]);
+        return i; // Return the index of the pivot
     }
 
 };
